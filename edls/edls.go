06b5adc7ab431043bbd6c46449c8e73657f46e36
Diego Urrego <diegourrg@gmail.com>
2024-07-05 21:20:55 -0500

    feat: edls project started. Consts definitions done and .gitkeep file deleted.

---
+++ edls/edls.go
@@ -0,0 +1,23 @@
+package main
+
+// file types
+const (
+	fileRegular = iota
+	fileDirectory
+	fileExecutable
+	fileCompress
+	fileImage
+	fileLink
+)
+
+// file extensions
+const (
+	exe = ".exe"
+	deb = ".deb"
+	zip = ".zip"
+	tar = ".tar"
+	rar = ".rar"
+	png = ".png"
+	jpg = ".jpg"
+	gif = ".gif"
+)
